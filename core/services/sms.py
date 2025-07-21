863e79cdfcf61b8fc484c7ecafd5e1f9f56b232c
A'zamov Samandar <admin@jscorp.uz>
2025-07-21 12:40:00 +0500

    update 111111

--- core/services/sms.py
+++ core/services/sms.py
@@ -9,8 +9,8 @@ class SmsService:
     @staticmethod
     def send_confirm(phone):
         # TODO: Deploy this change when deploying -> code = random.randint(1000, 9999) # noqa
-        # code = 1111
-        code = random.randint(100000, 999999)
+        code = 111111
+        # code = random.randint(100000, 999999)
 
         sms_confirm, status = models.SmsConfirm.objects.get_or_create(
             phone=phone, defaults={"code": code})
